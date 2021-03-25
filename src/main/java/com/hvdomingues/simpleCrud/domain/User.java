bba570ac0e5cc2867be5438335b21a2c62306d1c
hvdomingues <hvdomingues1@gmail.com>
2021-03-25 11:32:32 -0300

    user entity created

--- src/main/java/com/hvdomingues/simpleCrud/domain/User.java
+++ src/main/java/com/hvdomingues/simpleCrud/domain/User.java
@@ -1,11 +1,115 @@
 package com.hvdomingues.simpleCrud.domain;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+	
+	@Column(name = "user_login")
+	private String login;
+	
+	@Column(name = "user_full_name")
+	private String fullName;
+	
+	@Column(name = "user_birthday")
+	private Date birthday;
+	
+	@Column(name = "user_zip_code")
+	private String zipCode;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", login=" + login + ", fullName=" + fullName + ", birthday=" + birthday
+				+ ", zipCode=" + zipCode + "]";
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
